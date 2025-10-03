5d6467f795b3dc2f897da008ddb1ec65e499a1b3
abdoul aziz Diouf <133543436+smallziz18@users.noreply.github.com>
2025-10-03 20:58:16 +0000

    auth set up migrations (#52)
    * auth set up migrations
    
    * auth set up migrations
    
    ---------
    
    Co-authored-by: smallziz <abdaziz.diouf@astarex-ps.com>

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,9 +1,12 @@
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const location = sqliteTable("location", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   slug: text().notNull().unique(),
+  userId: int().notNull().references(() => user.id, { onDelete: "cascade" }),
   description: text(),
   lat: real().notNull(),
   lng: real().notNull(),
