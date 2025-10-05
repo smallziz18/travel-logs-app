640465b12552621c3556179f159250dc8e881da7
smallziz <abdaziz.diouf@astarex-ps.com>
2025-10-05 09:54:48 +0000

    sigout done

--- app/component/auth-btn.vue
+++ app/component/auth-btn.vue
@@ -5,17 +5,41 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button
-    :disabled="authStore.loading"
-    class="btn"
-    @click="authStore.signIn"
-  >
-    Sign in with GitHub
-    <span v-if="authStore.loading" class="loading loading-ring loading-xl" />
-    <Icon name="tabler:brand-github-copilot" size="26" />
-  </button>
+  <div>
+    <div v-if="!authStore.loading && authStore.user" class="dropdown bg-base-300 dropdown-end">
+      <div
+        tabindex="0"
+        role="button"
+        class="btn m-1"
+      >
+        <div class="avatar">
+          <div class="w-11 rounded-full mr-2">
+            <img :src="authStore.user.image || undefined" :alt="authStore.user.name">
+          </div>
+        </div>
+        {{ authStore.user.name }}
+      </div>
+      <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <li class="text-black dark:text-white">
+          <NuxtLink to="/sign-out">
+            <Icon name="tabler:logout" size="24" />
+            Sign out
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <button
+      v-else
+      :disabled="authStore.loading"
+      class="btn"
+      @click="authStore.signIn"
+    >
+      Sign in with GitHub
+      <span v-if="authStore.loading" class="loading loading-ring loading-xl" />
+      <Icon name="tabler:brand-github-copilot" size="26" />
+    </button>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
---
+++ app/pages/sign-out.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="container card bg-base-300 flex flex-col justify-center mt-4 min-h-72 text-center mx-auto">
+    <span class="loading loading-ring loading-xl" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
