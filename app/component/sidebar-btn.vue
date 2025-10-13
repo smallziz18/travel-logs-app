e14b7ca4b876105a3a1e107310641793fa9f7044
smallziz <abdaziz.diouf@astarex-ps.com>
2025-10-13 18:35:53 +0000

    setting up sidebar-btn.vue and setting it's st

---
+++ app/component/sidebar-btn.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+const route = useRoute();
+</script>
+
+<template>
+  <NuxtLink
+    :class=" { 'bg-base-200': route.path === props.href }"
+    :to="props.href"
+    class="flex gap-2 tooltip  tooltip-right p-2 hover:bg-base-300 hover:cursor-pointer justify-start"
+    :data-tip="!props.showLabel ? props.label : ''"
+  >
+    <Icon :name="props.icon" size="32" />
+    <span v-if="props.showLabel">{{ props.label }}</span>
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,9 +1,61 @@
 <script setup lang="ts">
+import SidebarBtn from "~/component/sidebar-btn.vue";
 
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem("isSidebarOpen", isSidebarOpen.value.toString());
+}
 </script>
 
 <template>
-  <h1>ypu a re logged in</h1>
+  <div class="flex flex-1 ">
+    <div class="bg-base-100 transition-all duration-300" :class="isSidebarOpen ? 'w-60' : 'w-16'">
+      <div class="flex justify-end" :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }">
+        <Icon
+          v-if="isSidebarOpen"
+          name="tabler:chevron-left"
+          size="42"
+          @click="toggleSidebar"
+        />
+        <Icon
+          v-else
+          name="tabler:chevron-right"
+          size="42"
+          @click="toggleSidebar"
+        />
+      </div>
+      <div class="flex flex-col">
+        <SidebarBtn
+          label="LOCATION"
+          icon="tabler:map"
+          href="/Dashboard"
+          :show-label="isSidebarOpen"
+        />
+        <SidebarBtn
+          label="ADD LOCATION"
+          icon="tabler:circle-plus-filled"
+          href=""
+          :show-label="isSidebarOpen"
+        />
+        <div class="divider" />
+        <SidebarBtn
+          label="SIGN OUT"
+          icon="tabler:logout-2"
+          href="/sign-out"
+          :show-label="isSidebarOpen"
+        />
+      </div>
+    </div>
+    <div class="flex-1 bg-base-300">
+      <h1>Dashboard</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
